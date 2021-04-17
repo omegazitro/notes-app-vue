d361650a61f00aef12acc176991d93afad7f3288
omegazitro <morti137@stu.southtexascollege.edu>
2021-04-17 10:08:28 -0500

    Added Notes Design

---
+++ src/components/Note.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-orange-400 text-orange-800 rounded text-center p-2 space-y-2">
+    <h3 class="text-2xl font-thin tracking-wide">Note Title</h3>
+    <p class="text-sm font-medium text-yellow-900">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ducimus
+      voluptatum ipsam optio nemo nihil molestiae dolore perspiciatis libero
+      hic, rerum accusantium vero sed culpa excepturi inventore dignissimos odit
+      ullam?
+    </p>
+    <div class="flex justify-end space-x-2">
+      <button class="hover:text-green-600"><mdi:playlist-edit /></button>
+      <button class="hover:text-red-600"><raphael:trash /></button>
+    </div>
+  </div>
+</template>
---
+++ src/components/Notes.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 container mx-auto">
+    <note v-for="n in 10" :key="n" />
+    <NoteAdd />
+  </div>
+</template>
