1cdaf793c0b6df38d78086405a150cb43fac2307
omegazitro <morti137@stu.southtexascollege.edu>
2021-04-17 08:51:01 -0500

    Inital Commit, project setup

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import ViteComponents from 'vite-plugin-components'
+import Icons, { ViteIconsResolver } from 'vite-plugin-icons'
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': 'src',
+    },
+  },
+  plugins: [
+    vue(),
+    ViteComponents({
+      customComponentResolvers: ViteIconsResolver({
+        componentPrefix: '',
+      }),
+    }),
+    Icons(),
+  ],
+})
